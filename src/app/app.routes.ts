9fed80305f69f9eb8d9561b96344fd57f8d2c48c
ahmedabdelhalemm <ahmedabdelhalem233@gmail.com>
2025-11-06 19:56:11 +0200

    add tour plans

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,10 @@
 import { Routes } from '@angular/router';
 import { LocationComponent } from './Components/location/location.component';
+import { TourPlanComponent } from './Components/tour-plan/tour-plan.component';
 import { HomeComponent } from './Components/home/home.component';
-import { NotfoundComponent } from './Components/notfound/notfound.component';
-import {ProductComponent} from "./Components/product/product.component";
-export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: LocationComponent },
 
-  { path: 'packages', component: ProductComponent },
-   {path: 'location', component: LocationComponent},
-  { path: '**', component: NotfoundComponent },
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'location', component: LocationComponent},
+  {path: 'tour-plan', component: TourPlanComponent},
 ];
