56c4a415a12cde5d21e7f19cde249735ee99e547
Hazem Khaled <zmyhazemkhaled@gmail.com>
2025-11-07 11:10:03 +0200

    update routes

--- src/app/Components/tour-plan/tour-plan.component.ts
+++ src/app/Components/tour-plan/tour-plan.component.ts
@@ -4,65 +4,64 @@ import { BookingFormComponent } from '../booking-form/booking-form.component';
 
 @Component({
   selector: 'app-tour-plan',
-  imports: [CommonModule,BookingFormComponent],
+  imports: [CommonModule, BookingFormComponent],
   templateUrl: './tour-plan.component.html',
-  styleUrl: './tour-plan.component.css'
+  styleUrl: './tour-plan.component.css',
 })
 export class TourPlanComponent {
-  activeTab: string = 'information';
-
   tourDays = [
     {
       day: '01',
       title: 'Day 1: Departure',
-      description: 'Qui Tempore Voluptate Qui Quia Commodi Rem Praesentium Alias Et Voluptates Officia Sed Molestiae Sint Et Voluptas Quia. Qui Itorum Repudiandae Dolorum Deborem.',
+      description:
+        'Qui Tempore Voluptate Qui Quia Commodi Rem Praesentium Alias Et Voluptates Officia Sed Molestiae Sint Et Voluptas Quia. Qui Itorum Repudiandae Dolorum Deborem.',
       amenities: [
         { icon: 'star', text: '5 Star Accommodation' },
         { icon: 'star', text: '5 Star Accommodation' },
-        { icon: 'breakfast', text: 'Breakfast' }
-      ]
+        { icon: 'breakfast', text: 'Breakfast' },
+      ],
     },
     {
       day: '02',
       title: 'Day 2: Visiting Zurich, Geneva And Zermatt',
-      description: 'Donec Quam Felis, Ultricies Nec, Pellentesque Eu, Pretium Quis, Sem. Nulla Consequat Massa Quis Enim. Donec Pede Justo, Fringilla Vel, Aliquet Nec, Vulputate Eget, Arcu. In Enim Justo, Rhoncus Ut, Imperdiet.',
+      description:
+        'Donec Quam Felis, Ultricies Nec, Pellentesque Eu, Pretium Quis, Sem. Nulla Consequat Massa Quis Enim. Donec Pede Justo, Fringilla Vel, Aliquet Nec, Vulputate Eget, Arcu. In Enim Justo, Rhoncus Ut, Imperdiet.',
       amenities: [
         { icon: 'star', text: '5 Star Accommodation' },
         { icon: 'breakfast', text: 'Breakfast' },
         { icon: 'star', text: '5 Star Accommodation' },
-        { icon: 'breakfast', text: 'Breakfast' }
-      ]
+        { icon: 'breakfast', text: 'Breakfast' },
+      ],
     },
     {
       day: '03',
       title: 'Day 3: Rest',
-      description: 'Donec Quam Felis, Ultricies Nec, Pellentesque Eu, Pretium Quis, Sem. Nulla Consequat Massa Quis Enim. Donec Pede Justo, Fringilla Vel, Aliquet Nec, Vulputate Eget, Arcu. In Enim Justo, Rhoncus Ut, Imperdiet.',
+      description:
+        'Donec Quam Felis, Ultricies Nec, Pellentesque Eu, Pretium Quis, Sem. Nulla Consequat Massa Quis Enim. Donec Pede Justo, Fringilla Vel, Aliquet Nec, Vulputate Eget, Arcu. In Enim Justo, Rhoncus Ut, Imperdiet.',
       amenities: [
         { icon: 'star', text: '5 Star Accommodation' },
-        { icon: 'breakfast', text: 'Breakfast' }
-      ]
+        { icon: 'breakfast', text: 'Breakfast' },
+      ],
     },
     {
       day: '04',
       title: 'Day 4: Historical Tour',
-      description: 'Donec Quam Felis, Ultricies Nec, Pellentesque Eu, Pretium Quis, Sem. Nulla Consequat Massa Quis Enim. Donec Pede Justo, Fringilla Vel, Aliquet Nec, Vulputate Eget, Arcu. In Enim Justo, Rhoncus Ut, Imperdiet.',
+      description:
+        'Donec Quam Felis, Ultricies Nec, Pellentesque Eu, Pretium Quis, Sem. Nulla Consequat Massa Quis Enim. Donec Pede Justo, Fringilla Vel, Aliquet Nec, Vulputate Eget, Arcu. In Enim Justo, Rhoncus Ut, Imperdiet.',
       amenities: [
         { icon: 'star', text: '5 Star Accommodation' },
         { icon: 'breakfast', text: 'Breakfast' },
         { icon: 'star', text: '5 Star Accommodation' },
-        { icon: 'breakfast', text: 'Breakfast' }
-      ]
+        { icon: 'breakfast', text: 'Breakfast' },
+      ],
     },
     {
       day: '05',
       title: 'Day 5: Return',
-      description: 'Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.',
-    }
+      description:
+        'Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.',
+    },
   ];
-
-  setActiveTab(tab: string) {
-    this.activeTab = tab;
-  }
 
   onFormSubmit(formData: any) {
     console.log('Form submitted:', formData);
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,21 @@ import { Routes } from '@angular/router';
 import { LocationComponent } from './Components/location/location.component';
 import { TourPlanComponent } from './Components/tour-plan/tour-plan.component';
 import { HomeComponent } from './Components/home/home.component';
+import { ProductComponent } from './Components/product/product.component';
+import { NotfoundComponent } from './Components/notfound/notfound.component';
+import { PackageDetailsComponent } from './Components/package-details/package-details.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'location', component: LocationComponent},
-  {path: 'tour-plan', component: TourPlanComponent},
+  { path: '', component: HomeComponent },
+  { path: 'packages', component: ProductComponent },
+{
+  path: 'packages-details',
+  component: PackageDetailsComponent,
+  children: [
+    { path: '', redirectTo: 'tour-plan', pathMatch: 'full' },
+    { path: 'tour-plan', component: TourPlanComponent },
+    { path: 'location', component: LocationComponent },
+  ],
+},
+  { path: '**', component: NotfoundComponent },
 ];
