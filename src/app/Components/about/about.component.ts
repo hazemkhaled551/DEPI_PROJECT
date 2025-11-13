04f4547935ac11cf2e999bcb3812060a819c5286
AhmedElhoseny01 <ae208059@gmail.com>
2025-11-14 00:44:05 +0200

    add about page and gallery

---
+++ src/app/Components/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [RouterLink, RouterOutlet],
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,31 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { LocationComponent } from './Components/location/location.component';
 import { TourPlanComponent } from './Components/tour-plan/tour-plan.component';
 import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
 import { ProductComponent } from './Components/product/product.component';
 import { NotfoundComponent } from './Components/notfound/notfound.component';
 import { PackageDetailsComponent } from './Components/package-details/package-details.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'packages', component: ProductComponent },
-{
-  path: 'packages-details',
-  component: PackageDetailsComponent,
-  children: [
-    { path: '', redirectTo: 'tour-plan', pathMatch: 'full' },
-    { path: 'tour-plan', component: TourPlanComponent },
-    { path: 'location', component: LocationComponent },
-  ],
-},
+  {
+    path: 'packages-details',
+    component: PackageDetailsComponent,
+    children: [
+      { path: '', redirectTo: 'tour-plan', pathMatch: 'full' },
+      { path: 'tour-plan', component: TourPlanComponent },
+      { path: 'location', component: LocationComponent },
+    ],
+  },
   { path: '**', component: NotfoundComponent },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
